c915b2bff85af8dfc6a4fedea61e0c56c83275d2
JanJelonkiewicz <jan.jelonkiewicz1@gmail.com>
2018-04-26 23:56:10 +0200

    Added project files

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import gui.Window;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		Window window = new Window();
+		window.start(primaryStage);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
